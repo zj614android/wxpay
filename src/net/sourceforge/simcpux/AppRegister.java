9982262122c99354cbab1970b8e61882730a83f3
2807738720@qq.com <2807738720@qq.com>
2018-04-02 11:19:17 +0800

    支付，官方，eclipse，直接run

---
+++ src/net/sourceforge/simcpux/AppRegister.java
@@ -0,0 +1,22 @@
+package net.sourceforge.simcpux;
+
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+//import com.tencent.mm.sdk.openapi.IWXAPI;
+//import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AppRegister extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		final IWXAPI msgApi = WXAPIFactory.createWXAPI(context, null);
+
+		// 将该app注册到微信
+		msgApi.registerApp(Constants.APP_ID);
+	}
+}
